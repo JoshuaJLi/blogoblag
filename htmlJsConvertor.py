cd5ddf3ec77e475bb54d17666c0f4d35f3a772ca
Joshua Li <JoshuaJLi@outlook.com>
2022-12-30 20:43:10 -0800

    Create conversion script from html to js for includes

---
+++ htmlJsConvertor.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+directory = ".\site\includes"
+
+# code adapted from https://stackoverflow.com/a/2212728
+for folder, subs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".html"):
+                with open(os.path.join(folder, filename), 'r') as src:
+                    jsFile = os.path.join(folder, os.path.basename(filename).split(".")[0] + ".js")
+                    with open(jsFile, 'w') as dest:
+                        compressedContent = src.read().replace('\n','').replace('\r','')
+                        dest.write("document.write('" + compressedContent + "')")
